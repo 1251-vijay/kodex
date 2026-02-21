6e1b118718430d1cbf503e25f8c5a59787261474
1251-vijay <vvijayv258@gmail.com>
2026-02-21 17:58:23 +0530

    01: project setup

---
+++ src/app/page.tsx
@@ -0,0 +1,14 @@
+import { Button } from "@/components/ui/button";
+
+
+export default function Home(){
+return (
+  <div className="text-red-500">
+  Hello world
+  <Button >
+    Get Started 
+  </Button>
+
+  </div>
+)
+}
